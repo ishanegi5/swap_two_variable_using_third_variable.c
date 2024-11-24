d3082270ebde12c1b1e461523890d1fb3a9a3f29
Isha Negi <itsishanegi@gmail.com>
2024-11-24 14:34:56 +0530

    Create swap_two_variable_using_third_variable.c

---
+++ swap_two_variable_using_third_variable.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{ int a=5,b=7;
+printf("Before swaping a=%d ,b=%d\n",a,b);
+  a=a+b;
+  b=a-b;
+  a=a-b;
+  printf("After swaping a=%d ,b=%d",a,b);
+   printf("\nName: Isha Negi \nRoll number: 24 CE 47\n");
+    return 0;
+}
